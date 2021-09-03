14ddcff341ec57705f68743c83b269b37cb84d48
Awerito <diego.i.munoz.viveros@gmail.com>
2021-09-02 20:42:15 -0400

    Change: is more fancy

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,10 +1,12 @@
 def to_rna(dna_strand):
-    
-    dna_strand = dna_strand.upper()
-    dna_strand = dna_strand.replace('A', 'U')
-    dna_strand = dna_strand.replace('T', 'A')
-    dna_strand = dna_strand.replace('G', 'X')
-    dna_strand = dna_strand.replace('C', 'G')
-    dna_strand = dna_strand.replace('X', 'C')
+
+    dna_strand = (
+        dna_strand.upper()
+        .replace("A", "U")
+        .replace("T", "A")
+        .replace("G", "X")
+        .replace("C", "G")
+        .replace("X", "C")
+    )
 
     return dna_strand
