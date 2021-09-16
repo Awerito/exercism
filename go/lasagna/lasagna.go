dfbd95fa3992b88a6bac093ddce0bf167477049b
Awerito <awe@pop-os.localdomain>
2021-09-15 23:50:22 -0300

    Add: go begin

---
+++ go/lasagna/lasagna.go
@@ -0,0 +1,17 @@
+package lasagna
+
+func OvenTime() int {
+    return 40
+}
+
+func RemainingOvenTime(time int) int {
+    return OvenTime() - time
+}
+
+func PreparationTime(layers int) int {
+    return 2 * layers
+}
+
+func ElapsedTime(layers int, time int) int {
+    return PreparationTime(layers) + time
+}
